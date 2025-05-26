ef7c7b7e1ceef37b739187a2a8307ef5f055fd67
Stefan Jansen van Rensburg <113289274+StefanJvRCodes@users.noreply.github.com>
2025-05-26 15:57:51 +0200

    Delete small core

--- core/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start MongoDB
-# Create directory for MongoDB data if it doesn't exist
-mkdir -p /data/db
-mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
-
-# Start Uvicorn
-uvicorn main:app --host 0.0.0.0 --port 8000
