ea8da4f4dd60e636ed81dc987488d2df02773dbb
Stefan Jansen van Rensburg <113289274+StefanJvRCodes@users.noreply.github.com>
2025-05-27 11:55:35 +0200

    Update producer.py

---
+++ Core/rabbitmq/producer.py
@@ -0,0 +1,19 @@
+import aio_pika
+import json
+import admin
+
+async def publish_message(event_type: str, message: dict):
+    connection = await aio_pika.connect_robust(admin.RABBITMQ_URL)
+    channel = await connection.channel()
+
+    exchange = await channel.declare_exchange("fanout_exchange", aio_pika.ExchangeType.FANOUT)
+
+
+    queue = await channel.declare_queue(event_type, durable=True)
+    await channel.default_exchange.publish(
+        aio_pika.Message(body=json.dumps(message).encode()),
+        routing_key=""
+    )
+
+    print(f"📤 Published message to fanout exchange: {event_type} -> {message}")
+    await connection.close()
