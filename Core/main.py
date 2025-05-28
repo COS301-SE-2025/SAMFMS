7353fdaca92471c55dc96d159cd40f36ff5f67d8
S JvR <s.jvr0829@gmail.com>
2025-05-28 09:31:19 +0200

    Bugfix, broadcasting added back

--- Core/main.py
+++ Core/main.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import uvicorn
 import asyncio
 import logging
+from rabbitmq import producer, consumer, admin
 from routes import user
 from routes import auth
 from routes import vehicle
@@ -42,6 +43,52 @@ app.include_router(vehicle.router)
 @app.get("/")
 async def root():
     return {"message": "SAMFMS API is running"}
+
+@app.on_event("startup")
+
+async def startup_event():
+
+    await admin.broadcast_topics()
+
+    async def safe_consume():
+
+        try:
+
+            await consumer.consume_messages("user_events")
+
+        except Exception as e:
+
+            logger.error(f"Error in consumer: {e}")
+
+
+
+    asyncio.create_task(safe_consume())
+
+
+
+
+
+@app.post("/send/")
+
+async def send_message(data: dict):
+
+    try:
+
+        await producer.publish_message("user_events", data)
+
+        return {"status": "message sent"}
+
+    except Exception as e:
+
+        logger.error(f"Failed to send message: {str(e)}")
+
+        raise HTTPException(status_code=500, detail="Failed to send message")
+
+
+
+
+
+#await producer.publish_message("user.created", {"id": 1, "name": "Alice"})
 
 
 
