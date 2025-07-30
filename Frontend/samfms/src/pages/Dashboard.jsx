262ee150b7fa616c52443392f1a0eb1b4fd1c765
S JvR <s.jvr0829@gmail.com>
2025-07-30 10:51:57 +0200

    Fixed previous erroneous fix

--- Frontend/samfms/src/pages/Dashboard.jsx
+++ Frontend/samfms/src/pages/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
         const response = await getVehicles();
 
 
-        setTotalVehicles(response.data.data.vehicles.length);
+        setTotalVehicles(response.data.data.vehicles.length || 0);
         //setLoadingVehicles
         //setLoadingAnalytics
         //setAnalytics
